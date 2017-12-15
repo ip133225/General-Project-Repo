9b78f0f0701a7b6f7524512d3585ffe72c02d79b
Chane Eckhardt <33033313+ceckhardt22@users.noreply.github.com>
2017-12-15 10:44:02 -0600

    update

---
+++ The Final/The Final/InGame.vb
@@ -0,0 +1,7 @@
+﻿Public Class InGame
+
+    Shared Function vb() As Integer
+        Throw New NotImplementedException
+    End Function
+
+End Class
---
+++ The Final/The Final/StartMenu.vb
@@ -0,0 +1,20 @@
+﻿Public Class StartMenu
+
+    Private Sub btnNew_Click(sender As Object, e As EventArgs) Handles btnNew.Click
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Dim rQuit As DialogResult
+        rQuit = MessageBox.Show("Are you sure you want to quit?", "Game Quit", MessageBoxButtons.YesNo)
+        If rQuit = Windows.Forms.DialogResult.No Then
+            MessageBox.Show("I knew you couldn't leave me yet. :3")
+        ElseIf rQuit = Windows.Forms.DialogResult.Yes Then
+            Me.Close()
+        End If
+    End Sub
+
+    Private Sub picMainMenu_Click(sender As Object, e As EventArgs) Handles picMainMenu.Click
+        'test
+    End Sub
+End Class
